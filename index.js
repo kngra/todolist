9abaecc1716d4d62d18717eec57035c91a4f8b2a
kngra <142641242+kngra@users.noreply.github.com>
2023-11-19 21:49:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach(todo => {
+        add(todo);
+    })
+}
+
 form.addEventListener("submit",function (event) {
     event.preventDefault();
     add();
 });
 
-function add() {
+function add(todo) {
     let todoText = input.value;
+
+    if (todo){
+        todoText = todo.text;
+    }
     if (todoText) {
         const li = document.createElement("li");
         li.innerText = todoText;
         li.classList.add("list-group-item");
+
+        if (todo && todo.completed) {
+            li.classList.add("text-decoration-line-through");
+        }
+
+        li.addEventListener("contextmenu", function
+        (event){
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
+
+        li.addEventListener("click", function () {
+            li.classList.toggle
+            ("text-decoration-line-through");
+            saveData();
+        });
+
         ul.appendChild(li);
         input.value = "";
         saveData();
@@ -21,5 +51,15 @@ function add() {
 
 function saveData(){
     const lists = document.querySelectorAll("li");
-    console.log(lists);
+    let todos = [];
+
+    lists.forEach(list => {
+        let todo = {
+            text: list.innerText,
+            completed: list.classList.contains
+            ("text-decoration-line-through")
+        };
+        todos.push(todo); 
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
 }

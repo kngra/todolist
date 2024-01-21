0da18245ba0a5aeae9f1803ded10be07007322e9
kngra <142641242+kngra@users.noreply.github.com>
2024-01-21 11:33:48 +0900

    Update index.js
    リスト（タブ）の追加

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+'use strict'; /* 厳格にエラーをチェック */
+
+{ /* ローカルスコープ */
+
+// DOM取得
+const tabMenus = document.querySelectorAll('.tab_list-item');
+console.log(tabMenus);
+
+// イベント付加
+tabMenus.forEach((tabMenu) => {
+  tabMenu.addEventListener('click', tabSwitch);
+})
+
+// イベントの処理
+function tabSwitch(e) {
+
+  // クリックされた要素のデータ属性を取得
+  const tabTargetData = e.currentTarget.dataset.tab;
+  // クリックされた要素の親要素と、その子要素を取得
+  const tabList = e.currentTarget.closest('.tab_list');
+  console.log(tabList);
+  const tabItems = tabList.querySelectorAll('.tab_list-item');
+  console.log(tabItems);
+  // クリックされた要素の親要素の兄弟要素の子要素を取得
+  const tabPanelItems = tabList.
+  nextElementSibling.querySelectorAll('.tab_panel-box');
+  console.log(tabPanelItems);
+
+  // クリックされたtabの同階層のmenuとpanelのクラスを削除
+  tabItems.forEach((tabItem) => {
+    tabItem.classList.remove('is-active');
+  })
+  tabPanelItems.forEach((tabPanelItem) => {
+    tabPanelItem.classList.remove('is-show');
+  })
+
+  // クリックされたmenu要素にis-activeクラスを付加
+  e.currentTarget.classList.add('is-active');
+  // クリックしたmenuのデータ属性と等しい値を持つパネルにis-showクラスを付加
+  tabPanelItems.forEach((tabPanelItem) => {
+    if (tabPanelItem.dataset.panel ===  tabTargetData) {
+      tabPanelItem.classList.add('is-show');
+    }
+  })
+
+}
+
+}
+
+
+
+
+
+
+
+
 document.getElementById("form").style.display = "none";
 
 document.getElementById("add").onclick = function() {
@@ -84,7 +140,7 @@ function add(todo) {
         deleteButton.style.margin = "5px"
         deleteButton.style.border = "solid 1px #c0c0c0";
         deleteButton.style.backgroundColor = "#f5f5f5";
-        deleteButton.innerText = "x";
+        deleteButton.innerText = "×";
 
         deleteButton.addEventListener("click", function() {
             li.remove();
